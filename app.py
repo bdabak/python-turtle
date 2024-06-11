9581a8a369a3647812fb98fe795114c54ddfa008
Bayram DABAK <bayram.dabak@smod.com.tr>
2024-06-11 15:15:35 +0300

    Second changes

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import turtle;
 
 myPen = turtle.Turtle();
-turtle.bgcolor("red");
+turtle.bgcolor("black");
 
 myPen.speed(1);
 
@@ -9,3 +9,14 @@ myPen.color('#285078', '#a0c8f0');
 myPen.begin_fill();
 myPen.circle(100);
 myPen.end_fill();
+
+myPen.begin_poly();
+myPen.goto(0,0)
+myPen.goto(0,10)
+myPen.goto(0,30)
+myPen.goto(0,40)
+myPen.goto(0,50)
+myPen.goto(0,50)
+myPen.goto(0,50)
+myPen.goto(0,60)
+myPen.end_poly();
