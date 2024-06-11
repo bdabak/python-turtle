506c7f6c35a08100c0fd556fbec90e8cb6167ac8
Bayram DABAK <bayram.dabak@smod.com.tr>
2024-06-11 15:10:56 +0300

    first submit

---
+++ app.py
@@ -0,0 +1,11 @@
+import turtle;
+
+myPen = turtle.Turtle();
+turtle.bgcolor("red");
+
+myPen.speed(1);
+
+myPen.color('#285078', '#a0c8f0');
+myPen.begin_fill();
+myPen.circle(100);
+myPen.end_fill();
